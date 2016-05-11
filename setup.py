3720106380b72fe5d3606a37822aba420a14358b
Mihail Stoyanov <screamerbg@users.noreply.github.com>
2016-05-11 21:56:54 +0100

    Change version number

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 setup(
     name="mbed-cli",
     packages=["mbed"],
-    version="0.1.5",
+    version="0.1.6",
     url='http://github.com/ARMmbed/mbed-cli',
     author='ARM mbed',
     author_email='support@mbed.org',
