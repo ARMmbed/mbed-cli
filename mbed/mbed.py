3adf9d7688c1fbca5f25d98b7dcd718606aa8264
Cruz Monrreal II <Cruz.Monrreal@gmail.com>
2017-11-29 18:18:19 -0600

    Added regex to remove inline oauth params from repo urls.

--- mbed/mbed.py
+++ mbed/mbed.py
@@ -1142,7 +1142,7 @@ class Repo(object):
                 self.rev if self.rev else ''))
         action("Updating reference \"%s\" -> \"%s\"" % (relpath(cwd_root, self.path) if cwd_root != self.path else self.name, ref))
         with open(self.lib, 'wb') as f:
-            f.write(ref + '\n')
+            f.write(re.sub(r'://.*@', '://', ref) + '\n')
 
     def rm_untracked(self):
         untracked = self.scm.untracked()
