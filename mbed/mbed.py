3082eb0422b4f34bdc670ec56b28a7c8a5e0c541
Laurence de Bruxelles <laurence.de-bruxelles@stfc.ac.uk>
2018-03-05 13:18:09 +0000

    Add profile to Program defaults
    Treat profile like toolchain and target, and save to the .mbed file if
    set at the command line, and get the value from the .mbed file if
    present

--- mbed/mbed.py
+++ mbed/mbed.py
@@ -1555,11 +1555,18 @@ class Program(object):
             error("Please specify toolchain using the -t switch or set default toolchain using command 'mbed toolchain'", 1)
         return tchain
 
-    def set_defaults(self, target=None, toolchain=None):
+    def get_profile(self, profile=None):
+        profile_cfg = self.get_cfg('PROFILE')
+        profile_cfg = profile_cfg.split() if profile_cfg else []
+        return profile if profile else profile_cfg
+
+    def set_defaults(self, target=None, toolchain=None, profile=None):
         if target and not self.get_cfg('TARGET'):
             self.set_cfg('TARGET', target)
         if toolchain and not self.get_cfg('TOOLCHAIN'):
             self.set_cfg('TOOLCHAIN', toolchain)
+        if profile and not self.get_cfg('PROFILE'):
+            self.set_cfg('PROFILE', (' ').join(profile))
 
     def get_macros(self):
         macros = []
@@ -2381,6 +2388,7 @@ def compile_(toolchain=None, target=None, profile=False, compile_library=False, 
 
     target = program.get_target(target)
     tchain = program.get_toolchain(toolchain)
+    profile = program.get_profile(profile)
     macros = program.get_macros()
 
     if compile_config:
@@ -2452,7 +2460,7 @@ def compile_(toolchain=None, target=None, profile=False, compile_library=False, 
                 if not reset_dev(detected['port']):
                     error("Unable to reset the target board connected to your system.\nThis might be caused by an old interface firmware.\nPlease check the board page for new firmware.", 1)
 
-    program.set_defaults(target=target, toolchain=tchain)
+    program.set_defaults(target=target, toolchain=tchain, profile=profile)
 
 
 # Test command
@@ -2484,6 +2492,7 @@ def test_(toolchain=None, target=None, compile_list=False, run_list=False, compi
 
     target = program.get_target(target)
     tchain = program.get_toolchain(toolchain)
+    profile = program.get_profile(profile)
     macros = program.get_macros()
     tools_dir = program.get_tools()
     build_and_run_tests = not compile_list and not run_list and not compile_only and not run_only
@@ -2554,7 +2563,7 @@ def test_(toolchain=None, target=None, compile_list=False, run_list=False, compi
                   + args,
                   env=env)
 
-    program.set_defaults(target=target, toolchain=tchain)
+    program.set_defaults(target=target, toolchain=tchain, profile=profile)
 
 
 # Export command
