ba6d45c602e7eac8c84b9be67ac69910702cdb3e
Laurence de Bruxelles <laurence.de-bruxelles@stfc.ac.uk>
2018-04-03 14:36:33 +0100

    Make profile config var local-only

--- mbed/mbed.py
+++ mbed/mbed.py
@@ -2731,6 +2731,9 @@ def config_(var=None, value=None, global_cfg=False, unset=False, list_config=Fal
         if global_cfg:
             # Global configuration
             g = Global()
+            if var == 'PROFILE':
+                action('profile is a local-only option')
+                return
             if unset:
                 g.set_cfg(var, None)
                 action('Unset global %s' % name)
@@ -2800,7 +2803,7 @@ def profile_(path=None, global_cfg=False):
     return config_('profile', (' ').join(path), global_cfg=global_cfg)
 
 @subcommand('profile',
-    dict(name='path', nargs='+', help='Default profile path. Example: mbed-os/tools/profiles/develop.json, ./profile-cxx11.json'),
+    dict(name='path', nargs='*', help='Default profile path. Example: mbed-os/tools/profiles/develop.json, ./profile-cxx11.json'),
     dict(name=['-G', '--global'], dest='global_cfg', action='store_true', help='Use global settings, not local'),
     help='Set or get default profile(s)\n\n',
     description=(
--- test/profile_test.py
+++ test/profile_test.py
@@ -14,20 +14,55 @@
 from util import *
 import re
 
-def assertprofile(mbed, profile):
-    expected = '[mbed] {}'.format(profile)
+@pytest.fixture
+def empty(mbed):
+    pquery(['python', mbed, 'config', 'root', '.'])
 
-    # write the profile
-    popen(['python', mbed, 'config', 'profile', profile])
-    # read the profile
-    output = pquery(['python', mbed, 'config', 'profile']).strip()
+def config(mbed, *args):
+    call = ('python', mbed) + args
+    return pquery(call)
 
-    assert output == expected
+def assertunset(mbed, *args):
+    assert config(mbed, *args).startswith(
+        '[mbed] No default {} set'.format(args[-1]))
 
-# Tests `mbed config profile`
-def testprofile(mbed):
-    # need to mark dir as an mbed program
-    popen(['python', mbed, 'config', 'root', '.'])
+def assertconfig(mbed, var, value):
+    if value is None:
+        assertunset(mbed, 'config', var)
+    else:
+        config(mbed, 'config', var, value)
+        assert config(mbed, 'config', var) == \
+            '[mbed] {}\n'.format(value)
 
+def assertprofile(mbed, value):
+    if value is None:
+        assertunset(mbed, 'profile')
+    else:
+        config(mbed, 'profile', value)
+        assert config(mbed, 'profile') == \
+            '[mbed] {}\n'.format(value)
+
+
+def test_profile(mbed, empty):
+    '''test `mbed profile`'''
+    assertprofile(mbed, None)
+    assertprofile(mbed, 'profile.json')
     assertprofile(mbed, 'mbed-os/tools/profiles/develop.json')
     assertprofile(mbed, 'mbed-os/tools/profiles/develop.json cxx11_profile.json')
+
+def test_global_profile(mbed, empty):
+    '''test `mbed profile --global`'''
+    assert config(mbed, 'profile', '--global', '~/.mbed_global_profile') \
+        == '[mbed] profile is a local-only option\n'
+
+def test_config_profile(mbed, empty):
+    '''test `mbed config profile`'''
+    assertconfig(mbed, 'profile', None)
+    assertconfig(mbed, 'profile', 'profile.json')
+    assertconfig(mbed, 'profile', 'mbed-os/tools/profiles/develop.json')
+    assertconfig(mbed, 'profile', 'mbed-os/tools/profiles/develop.json cxx11_profile.json')
+
+def test_config_global_profile(mbed):
+    '''attempting to configure a global profile should result in an error message'''
+    assert config(mbed, 'config', '--global', 'profile', '~/.mbed_global_profile') \
+        == '[mbed] profile is a local-only option\n'
