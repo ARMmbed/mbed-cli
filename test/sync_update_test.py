0bf0f88f51a0ddc0e29042bffe812240fbf660f9
Cruz Monrreal II <Cruz.Monrreal@gmail.com>
2018-05-10 12:05:04 -0500

    Added -vv flags to mbed commands run in pytest to  produce more useful failure messages

--- test/sync_update_test.py
+++ test/sync_update_test.py
@@ -15,40 +15,40 @@ from util import *
 # Tests if a 'mbed sync', commit, and 'mbed update' works on a simple file change
 def test_sync_update(mbed, testrepos):
     test1 = testrepos[0]
-    popen(['python', mbed, 'import', test1, 'testimport'])
+    popen(['python', mbed, 'import', test1, 'testimport', '-vv'])
 
     with cd('test1/test2'):
         with open('hello', 'w') as f:
             f.write('hello\n')
         popen([scm(), 'add', 'hello'])
-        popen(['python', mbed, 'sync'])
+        popen(['python', mbed, 'sync', '-vv'])
         mkcommit()
 
     with cd('test1'):
-        popen(['python', mbed, 'sync'])
+        popen(['python', mbed, 'sync', '-vv'])
         mkcommit()
 
     with cd('testimport'):
-        popen(['python', mbed, 'update'])
+        popen(['python', mbed, 'update', '-vv'])
 
     assert os.path.isfile('testimport/test2/hello')
 
 # Tests if removing a library is carried over sync/update
 def test_sync_update_remove(mbed, testrepos):
     test1 = testrepos[0]
-    popen(['python', mbed, 'import', test1, 'testimport'])
+    popen(['python', mbed, 'import', test1, 'testimport', '-vv'])
 
     with cd('test1/test2'):
         remove('test3')
-        popen(['python', mbed, 'sync'])
+        popen(['python', mbed, 'sync', '-vv'])
         mkcommit()
 
     with cd('test1'):
-        popen(['python', mbed, 'sync'])
+        popen(['python', mbed, 'sync', '-vv'])
         mkcommit()
 
     with cd('testimport'):
-        popen(['python', mbed, 'update'])
+        popen(['python', mbed, 'update', '-vv'])
 
     assertls(mbed, 'testimport', [
         "testimport",
@@ -58,19 +58,19 @@ def test_sync_update_remove(mbed, testrepos):
 # Tests if adding a library is carried over sync/update
 def test_sync_update_add(mbed, testrepos):
     test1 = testrepos[0]
-    popen(['python', mbed, 'import', test1, 'testimport'])
+    popen(['python', mbed, 'import', test1, 'testimport', '-vv'])
 
     with cd('test1/test2'):
         copy('test3', 'testcopy')
-        popen(['python', mbed, 'sync'])
+        popen(['python', mbed, 'sync', '-vv'])
         mkcommit()
 
     with cd('test1'):
-        popen(['python', mbed, 'sync'])
+        popen(['python', mbed, 'sync', '-vv'])
         mkcommit()
 
     with cd('testimport'):
-        popen(['python', mbed, 'update'])
+        popen(['python', mbed, 'update', '-vv'])
 
     assertls(mbed, 'testimport', [
         "testimport",
@@ -84,19 +84,19 @@ def test_sync_update_add(mbed, testrepos):
 # Tests if moving a library is carried over sync/update
 def test_sync_update_move(mbed, testrepos):
     test1 = testrepos[0]
-    popen(['python', mbed, 'import', test1, 'testimport'])
+    popen(['python', mbed, 'import', test1, 'testimport', '-vv'])
 
     with cd('test1/test2'):
         move('test3', 'testmove')
-        popen(['python', mbed, 'sync'])
+        popen(['python', mbed, 'sync', '-vv'])
         mkcommit()
 
     with cd('test1'):
-        popen(['python', mbed, 'sync'])
+        popen(['python', mbed, 'sync', '-vv'])
         mkcommit()
 
     with cd('testimport'):
-        popen(['python', mbed, 'update'])
+        popen(['python', mbed, 'update', '-vv'])
 
     assertls(mbed, 'testimport', [
         "testimport",
