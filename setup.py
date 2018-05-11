b5ff44804099395a9be2c70f83d20a99b785b071
Cruz Monrreal II <Cruz.Monrreal@gmail.com>
2018-05-10 23:20:57 -0500

    Updated CircleCI files: - Uses version 2 of CircleCI yaml - Capabile of running python jobs in parallel - Removes version check from setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 # Copyright (c) 2016 ARM Limited, All Rights Reserved
 # SPDX-License-Identifier: Apache-2.0
 
-# Licensed under the Apache License, Version 2.0 (the "License"); 
+# Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 
 # You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 
-# Unless required by applicable law or agreed to in writing, software 
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
 # either express or implied.
 
 import os
@@ -32,5 +32,5 @@ setup(
             'mbed-cli=mbed.mbed:main',
         ]
     },
-    python_requires='>=2.7.10, <3',
+    python_requires='>=2.7.10',
 )
