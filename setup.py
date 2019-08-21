2756f0a3de843da7c877cefd0f4080612a0d18d4
Mark Edgeworth <mark.edgeworth@arm.com>
2019-08-21 17:35:55 +0100

    Updated version to 1.10.1 (#912)
    * Updated version to 1.10.1
    * Minimum Python 3 version is 3.4, requires mbed-os-tools 0.0.9

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2016 ARM Limited, All Rights Reserved
+# Copyright (c) 2016-2019 ARM Limited, All Rights Reserved
 # SPDX-License-Identifier: Apache-2.0
 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,12 +18,12 @@ with open("README.md", "r") as fh:
 
 setup(
     name="mbed-cli",
-    version="1.10.0",
+    version="1.10.1",
     description="Arm Mbed command line tool for repositories version control, publishing and updating code from remotely hosted repositories (GitHub, GitLab and mbed.com), and invoking Mbed OS own build system and export functions, among other operations",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='http://github.com/ARMmbed/mbed-cli',
-    author='ARM mbed',
+    author='Arm mbed',
     author_email='support@mbed.org',
     packages=["mbed"],
     entry_points={
@@ -32,15 +32,20 @@ setup(
             'mbed-cli=mbed.mbed:main',
         ]
     },
-    python_requires='>=2.7.10,!=3.0.*,!=3.1.*,<4',
+    python_requires='>=2.7.10, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.0, !=3.4.1, !=3.4.2, <4',
     classifiers=(
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ),
     install_requires=[
         "pyserial>=3.0,<4.0",
-        "mbed-os-tools<0.1.0",
+        "mbed-os-tools>=0.0.9,<0.1.0",
     ]
 )
